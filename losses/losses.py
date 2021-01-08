bd46a12065c13394ffd7cbe50474a40896831d6d
Qing Li <dylan.liqing@gmail.com>
2021-01-07 21:45:20 -0800

    update

--- losses/losses.py
+++ losses/losses.py
@@ -40,10 +40,7 @@ class ConfidenceBasedCE(nn.Module):
         """
         # Retrieve target and mask based on weakly augmentated anchors
         weak_anchors_prob = self.softmax(anchors_weak) 
-        # max_prob, target = torch.max(weak_anchors_prob, dim = 1)
-        m = Categorical(probs=weak_anchors_prob)
-        target = m.sample()
-        max_prob = torch.exp(m.log_prob(target))
+        max_prob, target = torch.max(weak_anchors_prob, dim = 1)
         mask = max_prob > self.threshold 
         b, c = weak_anchors_prob.size()
         target_masked = torch.masked_select(target, mask.squeeze())
