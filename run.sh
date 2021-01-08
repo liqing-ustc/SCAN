4149f2ad3c3e174d43fdcc4196d1578a75136590
Qing Li <dylan.liqing@gmail.com>
2021-01-08 00:08:36 -0800

    update

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ export OUTPUT=outputs/$DATASET
 mkdir -p $OUTPUT/pretext
 mkdir -p $OUTPUT/scan
 mkdir -p $OUTPUT/selflabel
-python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_$DATASET.yml >$OUTPUT/pretext/train.log
-python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_$DATASET.yml >$OUTPUT/scan/train.log
-python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_$DATASET.yml >$OUTPUT/selflabel/train.log
-# export MODEL=$OUTPUT/selflabel/model.pth.tar_65
-# python eval.py --config_exp configs/selflabel/selflabel_hint.yml --model $MODEL  --save_match
-# python eval.py --config_exp configs/selflabel/selflabel_hint.yml --model ${MODEL}_match --visualize_prototypes
+# python simclr.py --config_env configs/env.yml --config_exp configs/pretext/simclr_$DATASET.yml >$OUTPUT/pretext/train.log
+# python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_$DATASET.yml >$OUTPUT/scan/train.log
+# python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_$DATASET.yml >$OUTPUT/selflabel/train.log
+export MODEL=$OUTPUT/selflabel/model.pth.tar_78.2
+python eval.py --config_exp configs/selflabel/selflabel_hint.yml --model $MODEL  --save_match
+python eval.py --config_exp configs/selflabel/selflabel_hint.yml --model ${MODEL}_match --visualize_prototypes
